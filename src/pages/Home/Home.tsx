e2e68f09dc149c66f5dfc873927dc05523516c6f
Vlad Levshuk <vlad1levshuk@gmail.com>
2024-08-29 20:54:23 +0300

    feat: add virtualization to optimize renders

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,32 +1,67 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useGetPostsQuery } from '../../api/postsApiSlice';
-import { Post } from '../../types';
-import { Spinner } from '../../components/Spinner/Spinner';
-import styles from './Home.module.scss';
+import styles from "./Home.module.scss";
+import { FixedSizeGrid as Grid, GridChildComponentProps } from "react-window";
+import AutoSizer, { Size } from "react-virtualized-auto-sizer";
+import { useNavigate } from "react-router-dom";
+import { Spinner } from "../../components/Spinner/Spinner";
+import { useGetPostsQuery } from "../../api/postsApiSlice";
 
-export const Home: React.FC = () => {
-  const { data: posts = [], error, isLoading } = useGetPostsQuery({});
+const Cell = ({
+  columnIndex,
+  rowIndex,
+  style,
+  data,
+}: GridChildComponentProps) => {
+  const { posts } = data;
   const navigate = useNavigate();
-
-  if (isLoading) return <Spinner />;
-
   const handleCardClick = (postId: number) => {
     navigate(`/posts/${postId}`);
   };
+  const postIndex = rowIndex * 4 + columnIndex;
+  const post = posts[postIndex];
+  if (!post) return null;
 
   return (
-    <div className={styles['home__container']}>
-      {posts.map((post: Post) => (
-        <div
-          key={post.id}
-          className={styles['home__card']}
-          onClick={() => handleCardClick(post.id)}
-        >
-          <div className={styles['home__card-title']}><strong>{post.title}</strong></div>
-          <div className={styles['home__card-description']}>{post.body}</div>
-        </div>
-      ))}
+    <div
+      key={post.id}
+      className={styles["home__card"]}
+      style={style}
+      onClick={() => handleCardClick(post.id)}
+    >
+      <div className={styles["home__card-title"]}>
+        <strong>{post.title}</strong>
+      </div>
+      <div className={styles["home__card-description"]}>{post.body}</div>
+    </div>
+  );
+};
+
+export const Home: React.FC = () => {
+  const { data: posts = [], error, isLoading } = useGetPostsQuery({});
+
+  if (isLoading) return <Spinner />;
+  console.log("posts-->", posts);
+  const columnCount = 4;
+  const rowCount = Math.ceil(posts.length / columnCount);
+
+  return (
+    <div className={styles["home__container"]}>
+      {!!posts.length && (
+        <AutoSizer>
+          {({ height, width }: Size) => (
+            <Grid
+              columnCount={columnCount}
+              columnWidth={width / columnCount}
+              height={height}
+              rowCount={rowCount}
+              rowHeight={350}
+              width={1440}
+              itemData={{ posts }}
+            >
+              {Cell}
+            </Grid>
+          )}
+        </AutoSizer>
+      )}
     </div>
   );
 };
